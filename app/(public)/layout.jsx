8e73b50a2ccd0be9be174527e77e3b5356ad53b0
DiwaLogann <diwakarsekar20@gmail.com>
2025-10-16 22:40:23 +0530

    16-10 evening

--- app/(public)/layout.jsx
+++ app/(public)/layout.jsx
@@ -3,12 +3,14 @@
 import { Toaster } from 'react-hot-toast';
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import Banner from '@/components/Banner';
 
 export default function PublicLayout({ children }) {
 
     return (
         <>
         <Toaster position="top-center" reverseOrder={false} />
+        <Banner/>
             <Navbar />
             {children}
         
