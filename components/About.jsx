e5c931dceff375ff125cddffba40381c632043ab
Krishnamoorthi T <ezhilkris002@gmail.com>
2025-10-17 15:24:53 +0530

    Change footer slogan section margin to bottom

--- components/About.jsx
+++ components/About.jsx
@@ -86,7 +86,7 @@ export default function About() {
 
       {/* Footer slogan section */}
       <div className="w-full flex justify-center mt-0">
-        <div className="bg-[#7C2A47] text-[#F5F5F5] mr-6 ml-6 py-2 px-4 sm:py-4 sm:px-8 text-center w-full sm:w-[1200px] rounded-lg shadow-md">
+        <div className="bg-[#7C2A47] text-[#F5F5F5] mb-7 mr-6 ml-6 py-2 px-4 sm:py-4 sm:px-8 text-center w-full sm:w-[1200px] rounded-lg shadow-md">
           <h2 className="text-xl sm:text-2xl font-semibold mb-2">
             Quality • Trust • Service
           </h2>
