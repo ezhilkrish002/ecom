c8ba4f99797d74442dc2fb6ae163861b3cd52a66
Krishnamoorthi T <ezhilkris002@gmail.com>
2025-10-22 17:19:10 +0530

    Adjust text sizes in About component

--- components/About.jsx
+++ components/About.jsx
@@ -87,10 +87,10 @@ export default function About() {
       {/* Footer slogan section */}
       <div className="w-full flex justify-center mt-0">
         <div className="bg-[#7C2A47] text-[#F5F5F5] mb-9 mr-6 ml-6 py-2 px-4 sm:py-4 sm:px-8 text-center w-full sm:w-[1200px] rounded-lg shadow-md">
-          <h2 className="text-[17px] sm:text-2xl font-semibold mb-2">
+          <h2 className="text-[18px] sm:text-2xl font-semibold mb-2">
             Quality • Trust • Service
           </h2>
-          <p className="text-[15px]  sm:text-xl">Driven by performance, powered by integrity.</p>
+          <p className="text-[14px]  sm:text-xl">Driven by performance, powered by integrity.</p>
         </div>
       </div>
     </div>
